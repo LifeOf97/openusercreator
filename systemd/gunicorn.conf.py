262c65f7fe2c7fa7bb775312fcc8a041c4b9c821
realestkma <kelvinmayoayeni@gmail.com>
2022-05-31 12:52:19 +0100

    Updated gunicorn config file

--- systemd/gunicorn.conf.py
+++ systemd/gunicorn.conf.py
@@ -1,13 +1,11 @@
 import multiprocessing
 from pathlib import Path
 
-
 # Base directory
 BASE_DIR = Path(__file__).parent.parent
 
-
 bind = ["127.0.0.1:8000"]
-# bind = "unix:/run/evtlfashion-auth.sock"
+# bind = "unix:/run/creator.sock"
 workers = multiprocessing.cpu_count() * 2 + 1
 reload_engine = "inotify"
 group = "realestKMA"
