2eec6c9f89e924884a4e2f79c41b7db1884bc8f3
realestkma <kelvinmayoayeni@gmail.com>
2022-06-04 12:41:45 +0100

    cu

--- src/settings.py
+++ src/settings.py
@@ -13,7 +13,7 @@ load_dotenv()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.environ('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
