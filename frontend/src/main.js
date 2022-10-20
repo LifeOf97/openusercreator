21b0552b7bc98e724c4da0e063729d174e43adfc
realestkma <kelvinmayoayeni@gmail.com>
2022-10-20 22:06:09 +0100

    cu

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -17,8 +17,8 @@ const app = createApp(App);
 
 // axios settings
 // axios.defaults.baseURL = "http://192.168.43.208:8080/";
-axios.defaults.baseURL = "http://127.0.0.1:8080/";
-// axios.defaults.baseURL = "https://openuser.xyz/";
+// axios.defaults.baseURL = "http://127.0.0.1:8080/";
+axios.defaults.baseURL = "https://openuser.xyz/";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true;
 axios.defaults.timeout = 9000;
