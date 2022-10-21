a4b1dbe887dd4cccbfb4dca72fd7b3589e0e9777
realestkma <kelvinmayoayeni@gmail.com>
2022-10-21 22:10:07 +0100

    cu

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -16,9 +16,8 @@ import "tippy.js/themes/translucent.css";
 const app = createApp(App);
 
 // axios settings
-// axios.defaults.baseURL = "http://192.168.43.208:8080/";
 axios.defaults.baseURL = "http://127.0.0.1:8080/";
-// axios.defaults.baseURL = "https://openuser.xyz/";
+axios.defaults.baseURL = "https://openuser.xyz/";
 axios.defaults.headers.common["Content-Type"] = "application/json";
 axios.defaults.withCredentials = true;
 axios.defaults.timeout = 9000;
